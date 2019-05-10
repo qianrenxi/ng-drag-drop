511acdb452da7ad3a73f5148c4383434dbbce77c
Tony <tonyeen@foxmail.com>
2019-05-10 08:48:58 +0800

    drop demo

--- src/app/drag-drop-demo/droppable-demo/photo-manager/photo-manager.component.ts
+++ src/app/drag-drop-demo/droppable-demo/photo-manager/photo-manager.component.ts
@@ -10,29 +10,64 @@ export class PhotoManagerComponent implements OnInit {
   photos = [
     {
       title: 'Demo 1',
-      thumbnail: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras1_min.jpg',
-      imgUrl: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras1_min.jpg'
+      thumbnail: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras_min.jpg',
+      imgUrl: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras.jpg',
+      isDeleted: false,
     },
     {
       title: 'Demo 1',
       thumbnail: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras2_min.jpg',
-      imgUrl: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras2_min.jpg'
+      imgUrl: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras.jpg',
+      isDeleted: false,
     },
     {
       title: 'Demo 1',
       thumbnail: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras3_min.jpg',
-      imgUrl: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras3_min.jpg'
+      imgUrl: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras.jpg',
+      isDeleted: false,
     },
     {
       title: 'Demo 1',
       thumbnail: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras4_min.jpg',
-      imgUrl: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras4_min.jpg'
+      imgUrl: 'https://jqueryui.com/resources/demos/droppable/images/high_tatras.jpg',
+      isDeleted: true,
     },
   ]
+
+  get gallery(): any[] {
+    return this.photos.filter(it => !it.isDeleted);
+  }
+
+  get trashed(): any[] {
+    return this.photos.filter(it => it.isDeleted);
+  }
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onTrashDrop(event) {
+    const { source } = event;
+    const { dragData } = source;
+    if (!!dragData) {
+      this.delete(dragData)
+    }
+  }
+
+  onGalleryDrop(event) {
+    const { source } = event;
+    const { dragData } = source;
+    if (!!dragData) {
+      this.recycle(dragData)
+    }
+  }
+
+  delete(item) {
+    item.isDeleted = true;
+  }
+
+  recycle(item) {
+    item.isDeleted = false;
+  }
 }
